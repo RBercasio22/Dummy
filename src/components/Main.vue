4c8a26b4e2858c9ce1153f6ba49d3450367c28b9
RB <RBercasio22@gmail.com>
2024-07-10 13:54:11 -0700

    First commit

---
+++ src/components/Main.vue
@@ -0,0 +1,29 @@
+
+
+<template>
+    <div class="top-nav">
+        <h1>Hello</h1>
+    </div>
+    <div class="left-side-nav">
+        <p>
+            <v-icon icon="mdi-account-arrow-down-outline"></v-icon>
+            
+            Dummy link</p>
+        <p>Dummy link</p>
+        <p>Dummy link</p>
+        <p>Dummy link</p>
+       
+    </div>
+
+</template>
+
+
+<script>
+
+export default {
+    name: 'Main',
+}
+
+
+
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,22 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <Main />
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import { defineComponent } from 'vue';
 
-export default {
+// Components
+//import HelloWorld from '../components/HelloWorld.vue';
+import Main from '../components/Main.vue';
+
+
+//  <HelloWorld />
+export default defineComponent({
   name: 'HomeView',
+
   components: {
-    HelloWorld
-  }
-}
+    Main
+    //HelloWorld,
+  },
+});
 </script>
